3750bcdcbb4d29f9e9c0fedf85db411135594595
Sivcan Singh <sivcansingh@me.com>
2018-10-12 14:44:21 +0530

    Added file extension support from requests

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
-const express = require('express');
-  app = express();
-  fs = require('fs');
-  shell = require('shelljs');
-  folderPath = './Responses/'; // Modify the folder path in which responses need to be stored
-  fileExtension = '.json'; // Change the file extension
+const express = require('express'),
+  app = express(),
+  fs = require('fs'),
+  shell = require('shelljs'),
+
+   // Modify the folder path in which responses need to be stored
+  folderPath = './Responses/',
+  defaultFileExtension = 'json', // Change the default file extension
   bodyParser = require('body-parser'),
   path = require('path');
 
+// Create the folder path in case it doesn't exist
 shell.mkdir('-p', folderPath);
 
-app.use(bodyParser.json({limit: '50mb', extended: true})); // Change limit according to your response size
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));  // Change limit accordingly
+ // Change the limits according to your response size
+app.use(bodyParser.json({limit: '50mb', extended: true}));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true })); 
 
-app.get('/', (req, res) => res.send('Hello, I write data to file.'));
+app.get('/', (req, res) => res.send('Hello, I write data to file. Send them requests!'));
 
 app.post('/write', (req, res) => {
-  let filePath = `${path.join(folderPath, req.body.requestName)}${fileExtension}`;
+  let extension = req.body.fileExtension || defaultFileExtension,
+    filePath = `${path.join(folderPath, req.body.requestName)}.${extension}`;
+
   fs.writeFile(filePath, req.body.responseData, (err) => {
     if (err) {
       console.log(err);
@@ -27,4 +33,7 @@ app.post('/write', (req, res) => {
   });
 });
 
-app.listen(3000, () => console.log('Postman-File App listening on port 3000!'))
+app.listen(3000, () => {
+  console.log('ResponsesToFile App is listening now! Send them requests my way!');
+  console.log(`Data is being stored at location: ${path.join(process.cwd(), folderPath)}`);
+});
