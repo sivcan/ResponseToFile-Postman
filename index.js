1807503b38b5f2d252fd6d2f6d81cfc9c36c6f4b
Jean-Marc Caudron <jean-marc.caudron@capgemini.com>
2023-03-16 11:55:59 +0100

    Add some sublime options

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const express = require('express'),
   shell = require('shelljs'),
 
    // Modify the folder path in which responses need to be stored
-  folderPath = './Responses/',
+  folderPath = './files/',
   defaultFileExtension = 'json', // Change the default file extension
   bodyParser = require('body-parser'),
-  DEFAULT_MODE = 'writeFile',
+  defaultEncoding ='utf-8',
   path = require('path');
 
 // Create the folder path in case it doesn't exist
@@ -21,7 +21,7 @@ app.get('/', (req, res) => res.send('Hello, I write data to file. Send them requ
 
 app.post('/write', (req, res) => {
   let extension = req.body.fileExtension || defaultFileExtension,
-    fsMode = req.body.mode || DEFAULT_MODE,
+    fsMode = req.body.mode ,
     uniqueIdentifier = req.body.uniqueIdentifier ? typeof req.body.uniqueIdentifier === 'boolean' ? Date.now() : req.body.uniqueIdentifier : false,
     filename = `${req.body.requestName}${uniqueIdentifier || ''}`,
     filePath = `${path.join(folderPath, filename)}.${extension}`,
@@ -37,7 +37,30 @@ app.post('/write', (req, res) => {
     }
   });
 });
+app.get('/read', (req, res) => {
+  let extension = req.body.fileExtension || defaultFileExtension,
+    fsMode = req.body.mode ,
+	encoding= req.body.encoding || defaultEncoding
+    uniqueIdentifier = req.body.uniqueIdentifier ? typeof req.body.uniqueIdentifier === 'boolean' ? Date.now() : req.body.uniqueIdentifier : false,
+    filename = `${req.body.requestName}${uniqueIdentifier || ''}`,
+    filePath = `${path.join(folderPath, filename)}.${extension}`,
+    options = req.body.options || undefined;
+	console.log(filePath);
 
+  fs[fsMode](filePath, encoding, (err,data) => {
+	if (data){
+		console.log("OK data retrieved");
+      res.send(data);
+    }  else  if (err) {
+      console.log("errror:"+err);
+      res.send('Error');
+    } else
+	{
+		console.log(req);
+		res.send('No data');
+	}
+  });
+});
 app.listen(3000, () => {
   console.log('ResponsesToFile App is listening now! Send them requests my way!');
   console.log(`Data is being stored at location: ${path.join(process.cwd(), folderPath)}`);
